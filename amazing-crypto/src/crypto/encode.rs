f1303d9c456c8e31ee16cd8ab9c5d3c9fe85cb5b
xutao <xutao@gmail.com>
2025-07-15 21:01:53 +0700

    init

--- amazing-crypto/src/crypto/encode.rs
+++ amazing-crypto/src/crypto/encode.rs
@@ -1,15 +1,12 @@
-use std::fs::{read, write};
 use crate::dict::unicode_crypto_dict::CRYPTO_CHARS;
 
-
-pub fn encode_file(input_path: &str, output_path: &str){
-    // 1. 读取文件内容（u8 数据）
-    let data = read(input_path).unwrap();
+pub fn encode(data:&[u8]) -> String {
     let data_len = data.len();
     let data_pairs = data_len / 2;
     let data_has_odd = data_len % 2 == 1;
+   
+    let mut encoded = String::with_capacity(data_pairs + 2);
     // 处理完整字节对
-    let mut encoded = String::new();
     for i in 0..data_pairs {
         let hi = data[2 * i];
         let lo = data[2 * i + 1];
@@ -25,7 +22,5 @@ pub fn encode_file(input_path: &str, output_path: &str){
         encoded.push(c);
         encoded.push(CRYPTO_CHARS[65536]);
     }
-    // 3. 写入为 UTF-8 文本文件
-    write(output_path, encoded).unwrap();
- 
+    encoded
 }
---
+++ amazing-crypto/src/crypto/file_decode.rs
@@ -0,0 +1,8 @@
+use std::fs::{read_to_string, write};
+use crate::crypto::decode::decode;
+
+pub fn decode_file(input_path: &str, output_path: &str){
+    let content = read_to_string(input_path).expect("Failed to read input file");
+    let bytes = decode(&content);
+    write(output_path, &bytes).expect("Failed to write output file");
+}
