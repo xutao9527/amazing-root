2b86643a304e2c1f6a1f6a9763f68e476b20ccf8
xutao <xutao@gmail.com>
2025-07-17 20:00:14 +0700

    init

--- amazing-crypto/src/lib.rs
+++ amazing-crypto/src/lib.rs
@@ -4,6 +4,7 @@ mod generator;
 
 #[cfg(test)]
 mod tests {
+    use std::path::Path;
     use crate::crypto::file_decode::decode_file;
     use crate::crypto::file_encode::encode_file;
     use crate::generator::unicode_crypto_generator::UnicodeCryptoGenerator;
@@ -15,7 +16,7 @@ mod tests {
         generator.run()
     }
 
-    // 生成加密图片
+    // 图片编码
     #[test]
     pub fn test_encode_img() {
         encode_file(
@@ -24,12 +25,39 @@ mod tests {
         );
     }
 
-    // 生成加密图片
+    // 图片解码
     #[test]
     pub fn test_decode_img() {
         decode_file(
             r"D:\codebase\rustProjects\amazing-root\amazing-web\static\12179.txt",
             r"D:\codebase\rustProjects\amazing-root\amazing-web\static\12179_decode.jpg",
         );
+    }
+
+    // 视频编码
+    #[test]
+    pub fn test_decode_video() {
+        let video_vec = vec![
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\test\test_dashinit.mp4",
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\test\test_dash1.m4s",
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\test\test_dash2.m4s",
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\test\test_dash3.m4s",
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\test\test_dash4.m4s",
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\test\test_dash5.m4s",
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\test\test_dash6.m4s",
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\test\test_dash7.m4s",
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\test\test_dash8.m4s",
+        ];
+
+        for input_path  in video_vec {
+            let input_path = Path::new(input_path);
+            let output_path = input_path.with_extension("txt");
+            encode_file(
+                input_path.to_str().unwrap(),
+                output_path.to_str().unwrap(),
+            );
+        }
+        
+      
     }
 }
