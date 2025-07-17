cb3eb563d7c6d97ff16144504013e6e192efef05
xutao <xutao@gmail.com>
2025-07-17 17:23:42 +0700

    init

--- amazing-wasm/src/lib.rs
+++ amazing-wasm/src/lib.rs
@@ -4,6 +4,8 @@ use amazing_crypto::crypto::decode::decode;
     打包说明
     cargo install wasm-pack
     wasm-pack build --target web --out-dir ../amazing-web/wasm
+    切片说明
+    MP4Box -dash 5000 -profile live test_h264.mp4
  */
 #[wasm_bindgen]
 pub fn decode_unicode(text:& str) -> Vec<u8> {
