6fa7077303956d72d86be089290e65a4abe965df
xutao <xutao@gmail.com>
2025-07-17 15:33:38 +0700

    init

--- amazing-crypto/src/lib.rs
+++ amazing-crypto/src/lib.rs
@@ -2,9 +2,10 @@ pub mod crypto;
 pub mod dict;
 mod generator;
 
-
 #[cfg(test)]
 mod tests {
+    use crate::crypto::file_decode::decode_file;
+    use crate::crypto::file_encode::encode_file;
     use crate::generator::unicode_crypto_generator::UnicodeCryptoGenerator;
 
     // 生成加密字典
@@ -12,5 +13,23 @@ mod tests {
     pub fn test_generator() {
         let generator = UnicodeCryptoGenerator::new();
         generator.run()
+    }
+
+    // 生成加密图片
+    #[test]
+    pub fn test_encode_img() {
+        encode_file(
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\12179.jpg",
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\12179.txt",
+        );
+    }
+
+    // 生成加密图片
+    #[test]
+    pub fn test_decode_img() {
+        decode_file(
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\12179.txt",
+            r"D:\codebase\rustProjects\amazing-root\amazing-web\static\12179_decode.jpg",
+        );
     }
 }
