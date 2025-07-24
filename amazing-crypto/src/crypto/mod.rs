57055ce931ed7e89932bde42abdbed6058e5624c
xutao <xutao@gmail.com>
2025-07-24 09:33:46 +0700

    init

--- amazing-crypto/src/crypto/mod.rs
+++ amazing-crypto/src/crypto/mod.rs
@@ -2,6 +2,7 @@ pub mod encode;
 pub mod decode;
 pub mod file_encode;
 pub mod file_decode;
+pub mod chacha_cipher;
 
 
 
--- amazing-crypto/src/lib.rs
+++ amazing-crypto/src/lib.rs
@@ -5,10 +5,27 @@ pub mod dict;
 #[cfg(test)]
 mod tests {
     use std::path::Path;
+    use crate::crypto::chacha_cipher::encrypt_decrypt_binary;
     use crate::crypto::file_decode::decode_file;
     use crate::crypto::file_encode::encode_file;
     use crate::dict::{char_to_index, generate_hardcoded, index_to_char};
 
+    // 加解密测试
+    #[test]
+    pub fn chacha_cipher(){
+        let key = [0u8; 32];
+        let nonce = [0u8; 12];
+        let mut buffer = b"hello chacha20 pure stream cipher".to_vec();
+        println!("Plaintext: {:?}", String::from_utf8_lossy(&buffer));
+        // 加密（原地修改 buffer）
+        encrypt_decrypt_binary(&key, &nonce, &mut buffer);
+        println!("Ciphertext: {:?}", buffer);
+        // 解密（原地再修改回明文）
+        encrypt_decrypt_binary(&key, &nonce, &mut buffer);
+        println!("Decrypted: {:?}", String::from_utf8_lossy(&buffer));
+        println!("Encryption and decryption successful!");
+    }
+    
     // 生成硬编码
     #[test]
     pub fn test_generate_hardcoded(){
